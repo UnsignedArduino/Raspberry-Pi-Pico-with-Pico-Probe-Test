65c0f2bf9cb3f71a7a3f4e4146b17035ac85d3b7
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-03-30 02:41:23 -0400

    Serial printing

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 
 void setup() {
+  Serial1.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+  Serial1.println("Hello, World!");
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
